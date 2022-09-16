4f91eda9a240dee970c16cd3670ed52d939ee9b0
Ddohree <dohrepl@gmail.com>
2022-09-16 19:55:26 +0200

    Inintial version

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int  maxINT = 50;
+        int minVal = Integer.MIN_VALUE;
+        int maxVal = Integer.MAX_VALUE;
+
+    }
+}
